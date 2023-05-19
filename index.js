eff7697cc5fec3f32ab50668ee976cc267486b7e
AhnafAlve <ahnafalve3331@gmail.com>
2023-05-20 05:06:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const chefs = require('./data/chefs.json');
+
+app.get('/', (req, res) => {
+    res.send('Chef is running....')
+});
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs); 
+})
+
+app.listen(port, () => {
+    console.log(`Dragon is runnig on port: ${port}`);
+})
+
